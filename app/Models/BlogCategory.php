634fcc960e7b0cc9176a26c179910f62f1839e92
itdok <itdok@yahoo.com>
2020-02-22 15:19:17 +0200

    lesson 45: category relations

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -5,9 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class BlogCategory
+ * @package App\Models
+ *
+ * @property-reed BlogCategory  $parentCategory
+ * @property-read string        $parentTitle
+ */
 class BlogCategory extends Model
 {
     use SoftDeletes;
+
+    const ROOTID = 1;
+
     protected $fillable
         = [
             'title',
@@ -15,4 +25,41 @@ class BlogCategory extends Model
             'parent_id',
             'description'
         ];
+
+    /**
+     * Подключить родительскую категорию
+     *
+     * @return BlogCategory
+     */
+    public function parentCategory()
+    {
+        return $this->belongsTo(BlogCategory::class, 'parent_id','id');
+    }
+
+    /**
+     *  Пример аксесуара (Accessor)
+     *
+     * @url https://laravel/com/docs/5/8/eloquent-mutators
+     *
+     * @return string
+     */
+    public function getParentTitleAttribute()
+    {
+        $title = $this->parentCategory->title
+            ?? ($this->isRoot()
+                ? 'Кореневая категория'
+                : '???');
+
+        return $title;
+    }
+
+    /**
+     * Является ли текущий объект корневым
+     *
+     * @return bool
+     */
+    public function isRoot()
+    {
+        return $this->id === BlogCategory::ROOTID;
+    }
 }
--- resources/views/blog/admin/categories/index.blade.php
+++ resources/views/blog/admin/categories/index.blade.php
@@ -28,7 +28,17 @@
                                         </a>
                                     </td>
                                     <td @if(in_array($item->parent_id, [0, 1])) style="color:#ccc" @endif>
-                                        {{ $item->parent_id }}{{-- $item->parentCategory->title --}}
+{{--                                        {{ $item->parentCategory->title ?? '?' }}--}}
+{{--                                        {{ optional($item->parentCategory)->title }}--}}
+{{--                                        {{--}}
+{{--                                            $item->parentCategory->title--}}
+{{--                                            ?? ($item->id === \App\Models\BlogCategory::ROOTID--}}
+{{--                                                ? 'Кореневая категория'--}}
+{{--                                                : '???')--}}
+{{--                                         }}--}}
+{{--                                        {{ $item->parent_title }}--}}
+                                        {{ $item->parentTitle }}
+
                                     </td>
                                 </tr>
                             @endforeach
